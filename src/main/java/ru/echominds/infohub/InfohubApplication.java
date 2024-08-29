010c2469a101552c08d2a66a5be75816b3185403
Raingor <kota-2019@inbox.ru>
2024-08-29 15:53:04 +0300

    init

---
+++ src/main/java/ru/echominds/infohub/InfohubApplication.java
@@ -0,0 +1,13 @@
+package ru.echominds.infohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfohubApplication.class, args);
+	}
+
+}

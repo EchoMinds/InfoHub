c7a3215cf60ddf67b392b1a2b84893238c6ff31d
RainGor <kota-2019@inbox.ru>
2024-10-10 22:31:45 +0300

    patch #1 update Security for Users * create tests * add new SecurityAuthorizationManager for UserService * upgrade logical SecurityAuthorizationManager

--- src/main/java/ru/echominds/infohub/services/UserService.java
+++ src/main/java/ru/echominds/infohub/services/UserService.java
@@ -2,10 +2,12 @@ package ru.echominds.infohub.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import ru.echominds.infohub.convertors.UserConvertor;
 import ru.echominds.infohub.domain.User;
+import ru.echominds.infohub.dtos.UpdatedUserDTO;
 import ru.echominds.infohub.dtos.UserDTO;
 import ru.echominds.infohub.exceptions.UserNotFoundException;
 import ru.echominds.infohub.repositories.UserRepository;
@@ -22,7 +24,16 @@ public class UserService {
     private final SecurityAuthorizationManager securityAuthorizationManager;
 
     public ResponseEntity<?> getUser() {
-        return securityAuthorizationManager.getCurrentUserOrAnonymous();
+        Object currentUserOrAnonymous = securityAuthorizationManager.getCurrentUserOrAnonymous();
+
+        if (currentUserOrAnonymous instanceof String) {
+            return new ResponseEntity<>(
+                    "Anonymous, please registration with your google account!", HttpStatus.OK);
+        }
+
+        User currentUser = (User) currentUserOrAnonymous;
+
+        return new ResponseEntity<>(userConvertor.convertUserToUserDTO(currentUser), HttpStatus.OK);
     }
 
     public UserDTO getUser(Long id) {
@@ -33,23 +44,29 @@ public class UserService {
     }
 
     public List<UserDTO> getAllUsers(PageRequest pageRequest) {
+        securityAuthorizationManager.checkAdminRole();
+
         return userRepository.findAll(pageRequest).stream().map(userConvertor::convertUserToUserDTO)
                 .collect(Collectors.toList());
     }
 
-    public void create(UserDTO createdUser) {
-        userRepository.save(
-                userConvertor.convertUserDTOToUser(createdUser)
-        );
-    }
-
-    public void updateUser(Long id, UserDTO userDto) {
+    public void updateUser(Long id, UpdatedUserDTO userDto) {
         User userFound = userRepository.findById(id).orElseThrow(UserNotFoundException::new);
 
-        userFound.setName(userDto.name() != null ? userDto.name() : userFound.getName());
-        userFound.setEmail(userDto.email() != null ? userDto.email() : userFound.getEmail());
-        userFound.setAvatar(userDto.avatar() != null ? userDto.avatar() : userFound.getAvatar());
+        updatedUserFields(userFound, userDto);
+    }
 
-        userRepository.save(userFound);
+    public void updateUser(UpdatedUserDTO userDto) {
+        User currentUser = securityAuthorizationManager.getCurrentUser();
+
+        updatedUserFields(currentUser, userDto);
+    }
+
+    private void updatedUserFields(User currentUser, UpdatedUserDTO userDto) {
+        currentUser.setName(userDto.name() != null ? userDto.name() : currentUser.getName());
+        currentUser.setEmail(userDto.email() != null ? userDto.email() : currentUser.getEmail());
+        currentUser.setAvatar(userDto.avatar() != null ? userDto.avatar() : currentUser.getAvatar());
+
+        userRepository.save(currentUser);
     }
 }
